738ef0d78f35297e9b66a42da212e6b15fd2e1ae
ozanndemirell <ozandemirel.93@gmail.com>
2022-06-21 21:21:17 +0300

    Needed to delete symbols like '

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -5,7 +7,6 @@ import pandas as pd
 
 def wordCount(str, counts):
     words = str.split()
-
     for word in words:
         if word in counts:
             counts[word] += 1
@@ -20,26 +21,36 @@ def orderDict(str):
 def saveAsExcel(df):
     df.to_excel("C:/Users/ozan.demirel/Desktop/thisIsPresidentTalking.xlsx")
 
-tccurl = 'https://www.tccb.gov.tr/'
-prompturl = tccurl + 'receptayyiperdogan/konusmalar/?&page=1'
-headers = {'User-Agent': 'my user agent(google)'}
-request = requests.get(prompturl, headers=headers)
-soup = BeautifulSoup(request.content, 'html.parser')
-prompt_div = soup.find('div', id='divContentList')
-a_s = prompt_div.find_all('a')
 
-urlList = []
 dic = {}
-for a in a_s:
-    url = a.get('href')
-    subject = a.text
-    url = tccurl + url
-    request = requests.get(url, headers=headers)
+forStatus = ''
+for i in range(1,999):
+    tccurl = 'https://www.tccb.gov.tr/'
+    prompturl = tccurl + 'receptayyiperdogan/konusmalar/?&page=' + str(i)
+    headers = {'User-Agent': 'my user agent(google)'}
+    request = requests.get(prompturl, headers=headers)
     soup = BeautifulSoup(request.content, 'html.parser')
-    speech = soup.find('div', id='divContentArea').text.replace('\n', '').replace('.', ' ').replace(',', ' ')\
-        .replace('?', ' ').replace('!', ' ').replace(':', ' ').replace(';', ' ').replace("'", " ").lower()
-    urlList.append([subject, speech])
-    wordCount(speech, dic)
+    prompt_div = soup.find('div', id='divContentList')
+    dls = prompt_div.find_all('dl')
+
+    for dl in dls:
+        date = dl.dt.text
+        if date[6:10] == '2022':
+            subject = dl.dd.a.text
+            url = dl.dd.a.get('href')
+            url = tccurl + url
+            request = requests.get(url, headers=headers)
+            soup = BeautifulSoup(request.content, 'html.parser')
+            speech = soup.find('div', id='divContentArea').text.replace('\n', '').replace('.', ' ').replace(',', ' ')\
+                .replace('?', ' ').replace('!', ' ').replace(':', ' ').replace(';', ' ').replace('"', '')\
+                .replace("'", "").replace("'", "").replace("-", " ").lower()
+            wordCount(speech, dic)
+        elif date[6:10] != '2022':
+            forStatus = 'Exit'
+            break
+    print(len(dic))
+    if forStatus == 'Exit':
+        break
 
 df = pd.DataFrame(data=orderDict(dic), columns=['Word', 'Count'])
 saveAsExcel(df)
